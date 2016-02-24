e669178dfb39b95a42bd8f45eee41b076f6195e7
Twist007 <h_bratoev@abv.bg>
2016-02-24 14:46:43 +0200

    code format

--- Homework/02. Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/MulArrSetsDict/08-Night-Life/NightLife.cs
+++ Homework/02. Advanced-CSharp-MultidimensionalArrays-Sets-Dictionaries-Homework/MulArrSetsDict/08-Night-Life/NightLife.cs
@@ -1,4 +1,16 @@
-﻿//Being a nerd means writing programs about night clubs instead of actually going to ones. Spiro is a nerd and he decided to summarize some info about the most popular night clubs around the world. He's come up with the following structure – he'll summarize the data by city, where each city will have a list of venues and each venue will have a list of performers.Help him finish the program, so he can stop staring at the computer screen and go get himself a cold beer.You'll receive the input from the console. There will be an arbitrary number of lines until you receive the string "END". Each line will contain data in format: "city;venue;performer". If either city, venue or performer don't exist yet in the database, add them. If either the city and/or venue already exist, update their info.Cities should remain in the order in which they were added, venues should be sorted alphabetically and performers should be unique (per venue) and also sorted alphabetically.Print the data by listing the cities and for each city its venues (on a new line starting with "->") and performers(separated by comma and space). Check the examples to get the idea.And grab a beer when you're done, you deserve it. Spiro is buying.
+﻿//Being a nerd means writing programs about night clubs instead of actually going to ones. 
+//Spiro is a nerd and he decided to summarize some info about the most popular night clubs 
+//around the world. He's come up with the following structure – he'll summarize the data by city,
+// where each city will have a list of venues and each venue will have a list of performers.
+//Help him finish the program, so he can stop staring at the computer screen and go get himself a 
+//cold beer.You'll receive the input from the console. There will be an arbitrary number of 
+//lines until you receive the string "END". Each line will contain data in format: "city;venue;performer".
+// If either city, venue or performer don't exist yet in the database, add them. If either the city 
+//and/or venue already exist, update their info.Cities should remain in the order in which they were added,
+// venues should be sorted alphabetically and performers should be unique (per venue) and also sorted 
+//alphabetically.Print the data by listing the cities and for each city its venues (on a new line starting 
+//with "->") and performers(separated by comma and space). Check the examples to get the idea.And grab a 
+//beer when you're done, you deserve it. Spiro is buying.
 
 using System;
 using System.Collections.Generic;
@@ -8,7 +20,7 @@ using System.Collections.Generic;
         static void Main()
         {
 
-        Dictionary<string, SortedDictionary<string, SortedSet<string>>> performencs = new Dictionary<string, SortedDictionary<string, SortedSet<string>>>();
+       var = new Dictionary<string, SortedDictionary<string, SortedSet<string>>>();
         string command = Console.ReadLine();
 
         while (command!="END")
